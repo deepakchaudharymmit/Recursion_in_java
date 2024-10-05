14514974854cef216285ee63ab2377d8a15200a1
Deepak Chaudhary <140324813+deepakchaudharymmit@users.noreply.github.com>
2024-10-06 00:24:29 +0530

    Create MaxElementInArray.java

---
+++ MaxElementInArray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class MaxElementInArray{
+
+    static int MaxElement(int[] arr,int index) {
+      
+        if (index==1)
+        {
+            return arr[0];
+        }
+        return Math.max(arr[index-1], MaxElement(arr,index-1));    
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size : ");
+        int n = sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(" Maxmim of Array element is : " + MaxElement(arr,n));
+    }
+}
